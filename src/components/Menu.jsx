cfff3690f403a93b0defa371765f7323ac7a9868
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2024-02-15 17:25:24 +0200

    small change

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,16 +5,19 @@ const Menu = () => {
   return (
     <main className='menu'>
       <h2>Our Menu</h2>
-      <p>
-        Authentic Italian cuisine. 6 creative dishes to choose from. All from
-        our stone oven, all organic, all delicious.
-      </p>
+
       {pizzaData.length > 0 ? (
-        <ul className='pizzas'>
-          {pizzaData.map((pizza, i) => (
-            <Pizza key={i} pizzaObj={pizza} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className='pizzas'>
+            {pizzaData.map((pizza, i) => (
+              <Pizza key={i} pizzaObj={pizza} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We &lsquo;re still working on our menu. Please comeback later</p>
       )}
